99d22cde5182d8c8bb82c56ec053b7cae27e1aca
emilianolee <yechan5172@daum.net>
2025-02-10 08:50:25 +0900

    5일차 커밋

---
+++ day04/py07_list_op2.py
@@ -0,0 +1,15 @@
+# py07_list_op2.py
+
+# append() 
+x = ['W', 'Y', 'Z']
+y = ['A', 'C', 'E']
+
+x.append(y)
+print(x)
+
+# extend()
+x = ['W', 'Y', 'Z']
+y = ['A', 'C', 'E']
+
+x.extend(y)
+print(x)
